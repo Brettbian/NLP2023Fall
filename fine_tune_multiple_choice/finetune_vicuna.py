71a2094cfe5f418afa02863082a4a090dd73da59
hanyuanz2000 <118020003+hanyuanz2000@users.noreply.github.com>
2023-11-28 14:42:07 -0500

    add training file

---
+++ fine_tune_multiple_choice/finetune_vicuna.py
@@ -0,0 +1,224 @@
+import argparse
+import yaml
+import bitsandbytes as bnb
+from datasets import load_dataset
+from functools import partial
+from utils import _get_max_length, load_model, preprocess_dataset
+import os
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, AutoPeftModelForCausalLM
+import torch
+from transformers import AutoTokenizer, set_seed, Trainer, TrainingArguments, BitsAndBytesConfig, \
+    DataCollatorForLanguageModeling, DataCollatorWithPadding, Trainer, TrainingArguments
+
+# logging.set_verbosity(logging.CRITICAL)
+import warnings
+warnings.filterwarnings("ignore")
+# warnings.simplefilter("always")
+
+#create a list of args to pass to the script
+parser = argparse.ArgumentParser()
+parser.add_argument("--base_model_name", type=str, default="lmsys/vicuna-7b-v1.5", help="model name")
+parser.add_argument("--dataset_name", type=str, help="dataset name")
+parser.add_argument("--output_dir", type=str, default="output", help="output directory")
+parser.add_argument("--batch_size", type=int, default=4, help="batch size")
+parser.add_argument("--learning_rate", type=float, default=1e-4, help="learning rate")
+parser.add_argument("--epochs", type=int, default=20, help="number of training epochs")
+parser.add_argument("--warmup_steps", type=int, default=5, help="warmup steps")
+parser.add_argument("--logging_steps", type=int, default=1, help="logging steps")
+parser.add_argument("--eval_strategy", type=str, default="epoch", help="evaluation strategy")
+parser.add_argument("--seed", type=int, default=42, help="random seed")
+parser.add_argument("--cot", type = bool, help = "using chain of thought training")
+
+def _parse_args():
+    args = parser.parse_args()
+    args_text = yaml.safe_dump(args.__dict__, default_flow_style=False)
+    return args, args_text
+
+
+def create_peft_config(modules):
+    """
+    Create Parameter-Efficient Fine-Tuning config for your model
+    :param modules: Names of the modules to apply Lora to
+    """
+    config = LoraConfig(
+        r=16,  # dimension of the updated matrices
+        lora_alpha=64,  # parameter for scaling
+        target_modules=modules,
+        lora_dropout=0.1,  # dropout probability for layers
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+
+    return config
+
+# SOURCE https://github.com/artidoro/qlora/blob/main/qlora.py
+def find_all_linear_names(model):
+    cls = bnb.nn.Linear4bit #if args.bits == 4 else (bnb.nn.Linear8bitLt if args.bits == 8 else torch.nn.Linear)
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+    if 'lm_head' in lora_module_names:  # needed for 16-bit
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
+
+def print_trainable_parameters(model, use_4bit=False):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        num_params = param.numel()
+        # if using DS Zero 3 and the weights are initialized empty
+        if num_params == 0 and hasattr(param, "ds_numel"):
+            num_params = param.ds_numel
+
+        all_param += num_params
+        if param.requires_grad:
+            trainable_params += num_params
+    if use_4bit:
+        trainable_params /= 2
+    print(
+        f"all params: {all_param:,d} || trainable params: {trainable_params:,d} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+def train(model, tokenizer, dataset, output_dir, trainargs):
+    # Apply preprocessing to the model to prepare it by
+    # 1 - Enabling gradient checkpointing to reduce memory usage during fine-tuning
+    model.gradient_checkpointing_enable()
+
+    # 2 - Using the prepare_model_for_kbit_training method from PEFT
+    model = prepare_model_for_kbit_training(model)
+
+    # Get lora module names
+    modules = find_all_linear_names(model)
+
+    # Create PEFT config for these modules and wrap the model to PEFT
+    peft_config = create_peft_config(modules)
+    model = get_peft_model(model, peft_config)
+
+    # Print information about the percentage of trainable parameters
+    print_trainable_parameters(model)
+    
+    pad_token_id = tokenizer.pad_token_id
+    model.config.pad_token_id = pad_token_id
+    
+    # Training parameters
+    trainer = Trainer(
+        model=model,
+        train_dataset=dataset['train'],
+        eval_dataset = dataset['validation'],
+        args = trainargs,
+        data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    )
+
+    model.config.use_cache = False  # re-enable for inference to speed up predictions for similar inputs
+
+    ### SOURCE https://github.com/artidoro/qlora/blob/main/qlora.py
+    # Verifying the datatypes before training
+    dtypes = {}
+    for _, p in model.named_parameters():
+        dtype = p.dtype
+        if dtype not in dtypes: dtypes[dtype] = 0
+        dtypes[dtype] += p.numel()
+    total = 0
+    for k, v in dtypes.items(): total+= v
+    for k, v in dtypes.items():
+        print(k, v, v/total)
+
+    # Launch training
+    print("Training...")
+    train_result = trainer.train()
+    print("Training done!")
+
+    metrics = train_result.metrics
+    trainer.log_metrics("train", metrics)
+    trainer.save_metrics("train", metrics)
+    trainer.save_state()
+    print(metrics)
+
+    print("Saving last checkpoint of the model...")
+    os.makedirs(output_dir, exist_ok=True)
+    trainer.model.save_pretrained(output_dir)
+
+    trainer.push_to_hub()
+    print(f"model successfully pushed to hub. username/{output_dir}")
+    
+    # # Free memory for merging weights
+    # del model
+    # del trainer
+    # torch.cuda.empty_cache()
+
+    return trainer
+
+def example_output(dataset, tokenizer, model):
+    model.config.use_cache = True
+    print("Example output from the finetuned model.")
+    input_text = dataset['validation'][0]['text'].split("</INST>")[0]
+    print(f"input text: {input_text}")
+    inputs=tokenizer.encode(input_text, return_tensors='pt').to('cuda')
+    outputs = model.generate(inputs=inputs, max_length=1000, num_return_sequences=1)
+    print(f"generated text:")
+    for i, output in enumerate(outputs):
+        print(f"{i}: {tokenizer.decode(output)}")
+
+def main():
+    args, args_text = _parse_args()
+    print(args_text)
+
+    #get all the args from the parser
+    base_model_name = args.base_model_name
+    dataset_name = args.dataset_name
+    output_dir = args.output_dir
+    batch_size = args.batch_size
+    learning_rate = args.learning_rate
+    num_train_epochs = args.epochs
+    warmup_steps = args.warmup_steps
+    logging_steps = args.logging_steps
+    eval_strategy = args.eval_strategy
+    seed = args.seed
+    cot = args.cot
+
+    # Save args to file
+    os.makedirs(output_dir, exist_ok=True)
+    with open(os.path.join(output_dir, "args.txt"), "w") as f:
+        f.write(args_text)
+
+    bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+    dataset = load_dataset(dataset_name)
+    model, tokenizer = load_model(base_model_name, bnb_config)
+    max_length = _get_max_length(model)
+    dataset = preprocess_dataset(tokenizer, max_length, seed, dataset, cot, dataset_name)
+
+    trainargs=TrainingArguments(
+            per_device_train_batch_size=batch_size,
+            evaluation_strategy = eval_strategy,
+            warmup_steps=warmup_steps,
+            learning_rate=learning_rate,
+            fp16=True,
+            logging_steps=logging_steps,
+            output_dir=output_dir,
+            optim="paged_adamw_8bit",
+            num_train_epochs=num_train_epochs,
+            push_to_hub = True
+    )
+
+    trainer = train(model, tokenizer, dataset, output_dir, trainargs)
+    example_output(dataset, tokenizer, model)
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
+
---
+++ fine_tune_multiple_choice/utils.py
@@ -0,0 +1,175 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import re
+from functools import partial
+import os
+import json
+
+def mkdir(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        pass
+
+def _get_max_length(model):
+    """
+    Get the maximum length setting from the model's configuration.
+
+    Args:
+        model: The model object.
+
+    Returns:
+        max_length (int): The maximum length setting.
+    """
+    max_length = None
+    for length_setting in ["n_positions", "max_position_embeddings", "seq_length"]:
+        max_length = getattr(model.config, length_setting, None)
+        if max_length:
+            print(f"Found max length: {max_length}")
+            break
+    if not max_length:
+        max_length = 1024
+        print(f"Using default max length: {max_length}")
+    return max_length
+
+def load_model(model_name, bnb_config):
+    """
+    Load the model and tokenizer.
+
+    Args:
+        model_name (str): The name of the model.
+        bnb_config: The quantization configuration.
+
+    Returns:
+        model: The loaded model.
+        tokenizer: The loaded tokenizer.
+    """
+    n_gpus = torch.cuda.device_count()
+    max_memory = f'{40960}MB'
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        device_map="auto", # dispatch efficiently the model on the available resources
+        max_memory = {i: max_memory for i in range(n_gpus)},
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True)
+
+    tokenizer.pad_token = tokenizer.eos_token
+
+    return model, tokenizer
+
+
+def _identify_dataset_type(dataset_name):
+    """
+    Identify the type of dataset based on its name.
+
+    Args:
+        dataset_name (str): The name of the dataset.
+
+    Returns:
+        dataset_type (str): The type of dataset.
+    """
+    if bool(re.search(r'McTest', dataset_name, re.IGNORECASE)):
+        return 'mctest'
+    elif bool(re.search(r'RACE', dataset_name, re.IGNORECASE)):
+        return 'race'
+    elif bool(re.search(r'CommonsenseQA', dataset_name, re.IGNORECASE)):
+        return 'commonsenseqa'
+    elif bool(re.search(r'ARC', dataset_name, re.IGNORECASE)):
+        return 'arc'
+    
+def preprocess_batch(batch, tokenizer, max_length):
+    """
+    Tokenizing a batch
+    """
+    return tokenizer(
+        batch["text"],
+        max_length=max_length,
+        truncation=True,
+        padding = True,
+    )
+
+def format_data(sample, dataset_name, cot):
+    dataset_type = _identify_dataset_type(dataset_name)
+    INSTRUCTION_KEY = "<s>[INST]"
+    INSTRUCTION_END = "</INST>"
+    END_OF_SENTENCE = "</s>"
+    if dataset_type == 'mctest':
+        #replace //newline with \n
+        sample['Story'] = sample['Story'].replace('\\newline', '')
+        prompt = f"Question: {sample['Question']} Based on the following article:\n{sample['Story']}.\nOptions: \n{sample['Options']}."
+        predicted_answer = sample['Predicted Answer']
+        actual_answer = sample['Actual Answer']
+    elif dataset_type == 'race':
+        prompt = f"Question: {sample['question']} Based on the following article:\n{sample['article']}.\nOptions: \n{sample['options']}."
+        predicted_answer = sample['predicted_answer']
+        actual_answer = sample['answer']
+    elif dataset_type =='commonsenseqa':
+        choices = sample['choices']
+        data_dict = json.loads(choices)
+        combined_list = [f"{label}. {text}" for label, text in zip(data_dict['label'], data_dict['text'])]
+        formatted_choices = " ".join(combined_list)
+        prompt = f"Question: {sample['question']}.\nOptions: \n{formatted_choices}."
+        predicted_answer = sample['predicted_answer']
+        actual_answer = sample['answerKey']
+    elif dataset_type == 'arc':
+        choices = sample['choices']
+        print(type(choices))
+        print(choices)
+        print(choices['label'])
+        try:
+            data = json.loads(choices)
+            print(data)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+        combined_list = [f"{label}. {text}" for label, text in zip(data_dict['label'], data_dict['text'])]
+        formatted_choices = " ".join(combined_list)
+        prompt = f"Question: {sample['question']}.\nOptions: \n{formatted_choices}."
+        predicted_answer = sample['predicted_answer']
+        actual_answer = sample['answerKey']
+    else:
+        raise Exception("Dataset type not recognized.")
+    if cot:
+        if predicted_answer == actual_answer:
+            sample['text'] = f"{INSTRUCTION_KEY}{prompt}{INSTRUCTION_END} Explanation: {sample['explanation']}{END_OF_SENTENCE}"
+        else:
+            sample['text'] = f"{INSTRUCTION_KEY}{prompt}{INSTRUCTION_END}{END_OF_SENTENCE}"
+    else: 
+        sample['text'] = f"{INSTRUCTION_KEY}{prompt}{INSTRUCTION_END} Answer: {actual_answer}{END_OF_SENTENCE}"
+    return sample
+
+# SOURCE https://github.com/databrickslabs/dolly/blob/master/training/trainer.py
+def preprocess_dataset(tokenizer: AutoTokenizer, max_length: int, seed, dataset, cot, dataset_name):
+    """Format & tokenize it so it is ready for training
+    :param tokenizer (AutoTokenizer): Model Tokenizer
+    :param max_length (int): Maximum number of tokens to emit from tokenizer
+    """
+
+    # Add prompt to each sample
+    print("Preprocessing dataset...")
+    _format_data = partial(format_data, dataset_name = dataset_name, cot = cot)
+    dataset = dataset.map(_format_data)
+
+    # Apply preprocessing to each batch of the dataset & and remove 'instruction', 'context', 'response', 'category' fields
+    _preprocessing_function = partial(preprocess_batch, max_length=max_length, tokenizer=tokenizer)
+    dataset = dataset.map(
+        _preprocessing_function,
+        batched=True,
+    )
+
+    # Filter out samples that have input_ids exceeding max_length
+    dataset = dataset.filter(lambda sample: len(sample["input_ids"]) < max_length)
+
+    # Set the tensor type
+    dataset.set_format("torch")
+
+    # Shuffle dataset
+    dataset = dataset.shuffle(seed=seed)
+
+    # Print one sample from dataset
+    print("Sample from dataset:")
+    print(dataset["train"][0])
+
+    return dataset
+
