d7523b39ba9ebb8533440f3f068f6e391f84c1ec
Brettbian <brettbian@yahoo.com>
2023-12-07 23:22:55 -0500

    add more to train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+srun --pty -c 2 --mem=15GB -t2:00:00 --gres=gpu:v100:1 /bin/bash
+
+singularity exec --bind /scratch --nv --overlay /scratch/yb970/capstone/overlay-25GB-500K.ext3:ro \
+  /scratch/work/public/singularity/cuda11.7.99-cudnn8.5-devel-ubuntu22.04.2.sif /bin/bash
+
+source /ext3/env.sh
+
+conda activate
+
+cd /scratch/yb970/NLP2023Fall
+
 python finetune.py --dataset_name BENBENBENb/RACE1000COT --output_dir brettbbb/race_cot_16 --epoch 20 --cot --train_size 16
 
 python finetune.py --dataset_name BENBENBENb/RACE1000COT --output_dir brettbbb/race_cot_32 --epoch 20 --cot --train_size 32
